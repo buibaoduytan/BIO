81732f899d3214bdc50d13c8a8bb4c675f81036c
duytann <alabtrap52@gmail.com>
2026-07-03 19:00:48 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./*.js"], // Nhận diện tất cả file HTML và JS ở thư mục gốc
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

de3977d50c88894281a289add7c263dfbf582074
duytann <alabtrap52@gmail.com>
2026-07-21 18:02:03 +0700

    animation_update

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./*.html", "./*.js"], // Nhận diện tất cả file HTML và JS ở thư mục gốc
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
